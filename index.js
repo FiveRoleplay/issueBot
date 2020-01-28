76d8ba2c5184d4063c9d845b65ac587a9f4cf9ee
Sumsun <sumsun93@gmail.com>
2020-01-28 01:38:57 +0100

    init project

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const Octokit = require("@octokit/rest");
+const config = require('./config.json');
+
+const client = new Discord.Client();
+const octokit = new Octokit({
+  auth: config.githubToken,
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.includes('!issue')) {
+    const args = msg.content.split('"');
+
+    const title = `${args[1]}`;
+    const body = args[3];
+
+    octokit.issues.create({
+      owner: config.usernameGithub,
+      repo: config.repoGithub,
+      title,
+      body: `<h3>Ouvert par ${msg.author.username}</h3><br/>${body}`,
+    }).then(() => {
+      msg.reply('Une issue a bien été créé.');
+    }).catch((err) => {
+      msg.reply("C'est cassé, contact un dev !");
+      console.log('ERROR', err);
+    })
+  }
+});
+
+client.login(config.botDiscordToken);
