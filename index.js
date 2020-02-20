c8651dd8dfcb238b6ce16d102d60e5e004dc0785
Sumsun <sumsun93@gmail.com>
2020-02-20 17:12:34 +0100

    Add get streams

--- index.js
+++ index.js
@@ -1,19 +1,98 @@
 const Discord = require('discord.js');
 const Octokit = require("@octokit/rest");
 const config = require('./config.json');
+const api = require("twitch-api-v5");
+const CronJob = require("cron").CronJob;
+
+api.clientID = "buemxzzsq0n2ttyfsyo080npaax118";
 
 const client = new Discord.Client();
 const octokit = new Octokit({
   auth: config.githubToken,
 });
 
+let streamsOnLive = [];
+
+const searchStreams = () => {
+  console.log('Lancement de la recherche de stream...');
+  let offset = 0;
+  let newStream = 0;
+  let currentStream = 0;
+  let streamEnd = 0;
+
+  api.streams.live({ game: "Grand Theft Auto V", language: "fr", stream_type: "live", offset }, async (err, res) => {
+    if(err) {
+      console.log(err);
+    } else {
+      const firstCall = res;
+      const maxOffset = Math.floor(firstCall._total / 25);
+      const streams = firstCall.streams;
+
+      if (maxOffset > 0) {
+        offset++;
+        while (offset < maxOffset) {
+          const test = await new Promise((resolve) => {
+              api.streams.live({ game: "Grand Theft Auto V", language: "fr", stream_type: "live", offset }, (err, res2) => {
+              if (err) {
+                console.log(err);
+                resolve([]);
+              }
+              else {
+                resolve(res2.streams);
+              }
+            })
+          });
+          streams.push(...test);
+          offset++;
+        }
+      }
+
+      const filterStreams = streams.filter(stream => stream.channel.status.toLowerCase().includes('ventura')).reduce((acc, item) => {
+        if (!acc.some(elmt => elmt.channel.url === item.channel.url)) return [...acc, item];
+        return acc;
+      }, []);
+
+      filterStreams.forEach(async stream => {
+        currentStream++;
+        if (!streamsOnLive.some(elmt => elmt.url === stream.channel.url)) {
+          const channel = client.channels.find('name', 'test');
+          const test = await channel.send(`Hey, y'a lui il stream ${stream.channel.url}`);
+          streamsOnLive.push({ url: stream.channel.url, message: test });
+          newStream++;
+        }
+      })
+
+      streamsOnLive.forEach(elmt => {
+        if (!filterStreams.some(stream => stream.channel.url === elmt.url)) {
+          streamsOnLive.find(elmt2 => elmt2.url === elmt.url).message.delete();
+          streamsOnLive = streamsOnLive.filter(elmt => elmt.url !== elmt.url);
+          streamEnd++;
+        }
+      })
+
+      console.log("Recherche terminée.");
+      console.log(`${currentStream} streams en cours dont ${newStream} streams démarrés`);
+      console.log(`${streamEnd} streams terminés`);
+    }
+  })
+}
+
 client.on('ready', () => {
   console.log('Connecté au Discord!');
+
+  // new CronJob("*/2 * * * *", searchStreams).start();
 
   // setInterval(function () {
   //   const test = client.users.find('discriminator', '0439');
   //   test.send({ files: ['./blobfish2.jpg'] });
   // }, 1000);
+
+  new CronJob("*/1 * * * *", () => {
+    const seb = client.users.find("discriminator", "2792");
+    console.log(seb);
+    seb.sendMessage("T'as cru t'étais qui?");
+  }).start();
+
 
   setInterval(function () {
     client.user.setPresence({
@@ -27,8 +106,11 @@ client.on('ready', () => {
   client.user.setStatus('available')
 });
 
-client.on('message', msg => {
-  if (msg.content.includes('!issue')) {
+client.on('message', async msg => {
+  if (msg.content.includes('!test')) {
+    
+  }
+  else if (msg.content.includes('!issue')) {
     if (msg.channel.name.toLowerCase() !== config.issueChannelName.toLowerCase()) return;
 
     const args = msg.content.split('"');
