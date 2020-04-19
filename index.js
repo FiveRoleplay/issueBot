0b9acceaedeb66f52a6fb2d87fe8859cb44f64f2
Sumsun <sumsun93@gmail.com>
2020-04-19 20:18:18 +0200

    :shrugV3:

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ const searchStreams = () => {
         else {
           const currentData = streamsOnLive.find(elmt => elmt.url === stream.channel.url);
 
-          if (currentData.message.embeds[0].title != stream.channel.status) {
+          if (currentData.message.embeds[0].title.trim() !== stream.channel.status.trim()) {
             const indexOf = streamsOnLive.indexOf(currentData);
             const newMessage = new Discord.MessageEmbed(currentData.message.embeds[0])
               .setTitle(stream.channel.status)
