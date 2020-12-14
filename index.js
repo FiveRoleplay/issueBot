fde2d9a2cd7048dd0258995cba0530b6d9ea8684
Mathieu <mathieu.dumontier@oclock.io>
2020-12-15 00:37:32 +0100

    Add sumsunnnn

--- index.js
+++ index.js
@@ -272,49 +272,64 @@ client.on('message', async msg => {
     const channel = client.channels.cache.find((test) => test.name === msg.channel.name);
     channel.send({ files: ['./boutonvert.png'] });
   }
-  else if (msg.content.includes('!interprete')) {
-    const channel = client.channels.cache.find((test) => test.name === msg.channel.name);
-    channel.send({ files: ['./interprete.mp4'] });
-  }
-  else if (msg.content.includes('!issue')) {
-    if (msg.channel.name.toLowerCase() !== config.issueChannelName.toLowerCase()) return;
+  else if (msg.content.includes("!interprete")) {
+    const channel = client.channels.cache.find(
+      (test) => test.name === msg.channel.name
+    );
+    channel.send({files: ["./interprete.mp4"]});
+  } else if (msg.content.includes("!sumsun?")) {
+    const channel = client.channels.cache.find(
+      (test) => test.name === msg.channel.name
+    );
+    channel.send({files: ["./sumsunnnn.mp4"]});
+  } else if (msg.content.includes("!issue")) {
+    if (
+      msg.channel.name.toLowerCase() !== config.issueChannelName.toLowerCase()
+    )
+      return;
 
     const args = msg.content.split('"');
 
     const title = `${args[1]}`;
     const body = args[3];
 
-    octokit.issues.create({
-      owner: config.usernameGithub,
-      repo: config.repoGithub,
-      title,
-      body: `<h3>Ouvert par ${msg.author.username}</h3><br/>${body}`,
-    }).then(() => {
-      msg.reply('Une issue a bien été créé.');
-    }).catch((err) => {
-      msg.reply("C'est cassé, contact un dev (Genre Sumsun le BG) !");
-      console.log('ERROR', err);
-    })
+    octokit.issues
+      .create({
+        owner: config.usernameGithub,
+        repo: config.repoGithub,
+        title,
+        body: `<h3>Ouvert par ${msg.author.username}</h3><br/>${body}`,
+      })
+      .then(() => {
+        msg.reply("Une issue a bien été créé.");
+      })
+      .catch((err) => {
+        msg.reply("C'est cassé, contact un dev (Genre Sumsun le BG) !");
+        console.log("ERROR", err);
+      });
   }
   // else if (msg.content.includes('!nico')) {
   //   const channel = client.channels.find('name', msg.channel.name);
   //   const nico = client.users.find('discriminator', '0001');
   //   channel.send(nico, { files: ['./blobfish2.jpg'] });
   // }
-  else if (msg.content.includes('!seb?')) {
-    const channel = client.channels.cache.find(elmt => elmt.name === msg.channel.name);
-    channel.send({ files: ['./seb.mp4'] });
-  }
-  else if (msg.content.includes('!mmk?')) {
-    const channel = client.channels.cache.find(elmt => elmt.name === msg.channel.name);
-    channel.send({ files: ['./mmk.mp4'] });
-  }
-  else if (msg.content.includes('!dams?')) {
-    const channel = client.channels.cache.find(elmt => elmt.name === msg.channel.name);
-    channel.send({ files: ['./dams.ogg'] });
-  }
-  else if (msg.content.includes('!dés')) {
-    const args = msg.content.split(' ');
+  else if (msg.content.includes("!seb?")) {
+    const channel = client.channels.cache.find(
+      (elmt) => elmt.name === msg.channel.name
+    );
+    channel.send({files: ["./seb.mp4"]});
+  } else if (msg.content.includes("!mmk?")) {
+    const channel = client.channels.cache.find(
+      (elmt) => elmt.name === msg.channel.name
+    );
+    channel.send({files: ["./mmk.mp4"]});
+  } else if (msg.content.includes("!dams?")) {
+    const channel = client.channels.cache.find(
+      (elmt) => elmt.name === msg.channel.name
+    );
+    channel.send({files: ["./dams.ogg"]});
+  } else if (msg.content.includes("!dés")) {
+    const args = msg.content.split(" ");
     const numberOfDice = parseInt(args[1]) || 1;
 
     if (numberOfDice > 195) {
@@ -323,16 +338,20 @@ client.on('message', async msg => {
     }
 
     const stringNumber = [
-      ':one:',
-      ':two:',
-      ':three:',
-      ':four:',
-      ':five:',
-      ':six:'
-    ]
+      ":one:",
+      ":two:",
+      ":three:",
+      ":four:",
+      ":five:",
+      ":six:",
+    ];
 
-    const isDev = msg.member.roles.cache.some(({ name }) => name.toLowerCase() === 'dev');
-    const isModo = msg.member.roles.cache.some(({ name }) => name.toLowerCase() === 'modération');
+    const isDev = msg.member.roles.cache.some(
+      ({name}) => name.toLowerCase() === "dev"
+    );
+    const isModo = msg.member.roles.cache.some(
+      ({name}) => name.toLowerCase() === "modération"
+    );
 
     if (numberOfDice && numberOfDice > 0) {
       let text = "Resultat: ";
@@ -347,8 +366,7 @@ client.on('message', async msg => {
         total += random;
         if (i !== numberOfDice - 1) {
           text += `${stringNumber[random - 1]} + `;
-        }
-        else if (i === numberOfDice - 1) {
+        } else if (i === numberOfDice - 1) {
           text += `${stringNumber[random - 1]}`;
 
           if (numberOfDice > 1) {
@@ -357,7 +375,7 @@ client.on('message', async msg => {
         }
       }
 
-      msg.reply(text)
+      msg.reply(text);
     }
   }
 });
